569ac58b01846cdec878088a660895e18c7d02fe
luizdopc <luizotaviosoares@outlook.com>
2018-12-03 18:33:19 -0200

    install 0.2 (descomentar)

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-sudo apt-get install ncbi-blast+
+#mkdir blast
+#cd blast;wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.7.1/ncbi-blast-2.7.1+-x64-linux.tar.gz
+#cd blast;tar -zxvf ncbi-blast-2.7.1+-x64-linux.tar.gz
+#cd blast;rm -rf ncbi-blast-2.7.1+-x64-linux.tar.gz
 
+cont=0
 while read line; do
-	blastp -query proteina -db nr -remote -entrez_query "$line" -outfmt 7 > LA\ FILES/"$line".la
+	echo GERANDO ARQUIVOS DE ENTRADA
+	echo \#"cont" GERANDO ARQUIVO PARA ESPECIE "$line"
+	blast/ncbi-blast-2.7.1+/bin/blastp -query proteina -db nr -remote -entrez_query "$line" -outfmt 7 > LA\ FILES/"$line".la
 done < especies
+echo FIM DO PROCESSO
 
