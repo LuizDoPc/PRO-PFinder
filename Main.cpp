4f242621d25dabd54026bd714024f4ccf52e9480
LuizDoPc <luizotaviosoares@outlook.com>
2018-12-04 14:45:10 -0200

    gera estruturas

--- Main.cpp
+++ Main.cpp
@@ -21,24 +21,39 @@ vector<string> open(string path = "LA FILES") {
 
 int main(int argc, char const *argv[]){
     
-    //ABB *abb = new ABB();
-    //TabelaHash *th = new TabelaHash();
+    ABB *abb = new ABB();
+    TabelaHash *th = new TabelaHash(194);
 
     
     vector<string> f;
 
-    f = open(); // or pass which dir to open
+    f = open();
 
-    fstream file("LA FILES/"+f[2]);
+    int k = 0;
+    for(vector<string>::iterator it =  f.begin(); it != f.end() and k < 5; it++, k++){
+        ifstream file("LA FILES/"+*it);
+        if(file.is_open()){
+            string line;
 
-    if(file.is_open()){
-        string line;
-        while ( getline (file, line) ){
-            cout << line << endl;
+            while(getline(file, line)){
+                if(line[0] != '#'){
+                    int cont = 0;
+                    string accession = "";
+                    for(int i=0; i<line.size(); i++){
+                        if(line[i] == '\t'){ 
+                            cont ++;
+                        }
+                        if(line[i] != '\t' and cont == 1){
+                            accession += line[i];
+                        }
+                    }
+                    
+                    int pos = th->insere(accession);
+                    abb->Inserir(*th, pos);
+                    //cout << accession << endl;
+                }    
+            }
         }
-    }
-    else{
-        cerr << "Erro" <<endl;
     }
     return 0;
 }
