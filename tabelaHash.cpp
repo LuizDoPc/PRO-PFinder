d40955685143a88e3a7c00fc7a476a64fe4ac5ed
LuizDoPc <luizotaviosoares@outlook.com>
2018-12-04 08:58:24 -0200

    divide tabelahash em .cpp e .h

--- tabelaHash.cpp
+++ tabelaHash.cpp
@@ -9,28 +9,6 @@ int funcaoHash(string s, int M) {
     return h;
 }
 
-class TabelaHash {
-    private:
-        // vetor de ponteiros de nÃ³s
-        NohHash** elementos;
-        int capacidade;
-    public:
-        // construtor padrÃ£o
-        TabelaHash(int cap = 100);
-        // destrutor
-        ~TabelaHash();
-        // insere um valor v com chave c
-        void insere(string c, string v);
-        // recupera um valor associado a uma dada chave
-        string recupera(string c);
-        // altera o valor associado a uma chave
-        void altera(string c, string v);
-        // retira um valor associado a uma chave
-        void remove(string c);
-        // percorrendo a tabela hash (para fins de debug)
-        void percorre();
-};
-
 // construtor padrÃ£o
 TabelaHash::TabelaHash(int cap) {
     elementos = new NohHash*[cap];
