764d0526d45dd6fe9214ec277092b2e35eeac3bc
luizdopc <luizotaviosoares@outlook.com>
2018-12-11 10:24:10 -0200

    att install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
-#mkdir blast
-#cd blast;wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.7.1/ncbi-blast-2.7.1+-x64-linux.tar.gz
-#cd blast;tar -zxvf ncbi-blast-2.7.1+-x64-linux.tar.gz
-#cd blast;rm -rf ncbi-blast-2.7.1+-x64-linux.tar.gz
+#mkdir blast cd blast;wget 
+#ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.7.1/ncbi-blast-2.7.1+-x64-linux.tar.gz 
+#cd blast;tar -zxvf ncbi-blast-2.7.1+-x64-linux.tar.gz cd 
+#blast;rm -rf ncbi-blast-2.7.1+-x64-linux.tar.gz
 
 echo GERANDO ARQUIVOS DE ENTRADA
 
-cont=1
+cont=0
 
 while read line; do
+	cont=$((cont+1))
 	echo \#"$cont" GERANDO ARQUIVO PARA ESPECIE "$line"
-	blast/ncbi-blast-2.7.1+/bin/blastp -query proteina -db nr -remote -entrez_query "$line" -outfmt 7 > LA\ FILES/"$line".la
-	cont = ($(cont)+1)
+	tosave="$(echo $line | tr -d /)"
+	tosave="$(echo $tosave | tr ' ' _)"
+	echo SALVANDO ARQUIVO COMO "$tosave".la
+	blast/ncbi-blast-2.7.1+/bin/blastp -query proteina -db nr -remote -entrez_query "$line" -outfmt 7 > LA\ FILES/"$tosave".la
 done < especies
 echo FIM DO PROCESSO
 
