e69d5346d0627c068b7325ab30941f43a8e8d2d0
Luiz Soares <luizotaviosoares@outlook.com>
2019-06-09 20:04:57 -0300

    update script

--- module3/getSeq.sh
+++ module3/getSeq.sh
@@ -1,7 +1,7 @@
 dir=("prnA" "prnB" "prnC" "prnD")
 for f in "${dir[@]}"
 do  
-    mkdir fasta$ffasta
+    mkdir fasta$f
     while read line; do
         wget -q -O fasta$f/$line.fasta "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id="+$line+"&db=protein&report=fasta&extrafeat=null&conwithfeat=on&hide-cdd=on&retmode=html&withmarkup=on&tool=portal&log$=seqview&maxdownloadsize=1000000"
     done < $f
